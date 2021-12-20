0e93e87929bfb8ef9aa357c63fa22d2a8f6388d0
noavrd <noavrd@gmail.com>
2021-12-20 16:14:02 +0200

    fix image artwork

--- src/components/artworks/SingleArtwork.js
+++ src/components/artworks/SingleArtwork.js
@@ -1,13 +1,19 @@
 export default function singleArtwork({ artwork }) {
   const pic =
-    artwork.default_img.folder + '/' + artwork.default_img.compressed_file_name;
+    'https://api.eden-gallery.com/artworks' +
+    artwork.default_img.folder +
+    '/' +
+    artwork.default_img.compressed_file_name;
   return (
     <div className="single-artwork">
-      <img
-        src={`https://api.eden-gallery.com/artworks${pic}`}
-        alt={artwork.name}
+      <div
         className="pic-artwork"
-      />
+        style={{
+          backgroundImage: `url(${pic}) `,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'cover',
+        }}></div>
+
       <h4 className="artwork-name">{artwork.name}</h4>
       <div className="artwork-size">{artwork.full_size}</div>
     </div>
